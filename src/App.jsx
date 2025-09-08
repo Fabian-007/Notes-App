4f85decee854968720a140ca994dab8a1a1dd84e
Fabian Nobert <fabiannobert.007@gmail.com>
2025-09-08 16:20:54 +0100

    Add hands on pratice of rendering collection, form handling, controlled compont and filtering display of element

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import Note from './components/Note';
+
+
+const App = (props) => {
+  console.log("notes props", props);
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] =useState('a new note...')
+  const [showAll, setShowAll] = useState(true)
+
+
+
+  const notesToShow = showAll
+  ? notes : notes.filter(note => note.important)
+
+  const addNote = (e) => {
+   e.preventDefault()
+   const noteObject = {
+    content: newNote,
+    important: Math.random() < 0.5,
+    id: String(notes.length +1)
+  }
+
+  if (newNote.trim() === '') {
+    setError('Note cannot be empty!');
+    return; // stop the function
+  }
+
+  setNotes(notes.concat(noteObject))
+  setNewNote('')
+
+  //  console.log('button clicked', e.target)
+  //  console.log('button clicked', e)
+  notes.forEach(note => {
+   console.log(`id: ${note.id}, type: ${typeof note.id}`) 
+  });
+  }
+  const handleNoteChange = (e) => {
+    const inputValue = e.target.value
+    console.log('input field value:', inputValue)
+    setNewNote(inputValue)
+
+    
+
+  
+  
+  }
+    return (
+    <>
+      <h1>Notes</h1>
+      <button onClick={() => setShowAll(!showAll)}>
+        show{showAll? 'important' : 'all'}
+      </button>
+      <ul>
+        {notesToShow.map((note) => (
+        //refactored to have a notes component 
+        <Note key={note.id} note = {note}/>
+      ))}
+      </ul>
+
+      <form onSubmit={addNote}>
+        <input 
+        value = {newNote}
+        onChange={handleNoteChange}/>
+        <button type='submit'>save</button>
+      </form>
+    </>
+  );
+};
+
+export default App;
